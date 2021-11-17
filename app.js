151957f2d1b3260e0e0a3f2bfa07044047addec9
Mazz Ghani <mazzg794@gmail.com>
2021-11-17 16:13:45 -0600

    partially done a2

--- app.js
+++ app.js
@@ -1,19 +1,68 @@
 const { response } = require('express')
 const express= require('express')
 const app=express()
+const mysql= require('mysql')
+const PATH = __dirname + "/views/"
+
+const connector= mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database:'assignment'
+})
+
+connector.connect((err) => {
+        if(err){
+            throw(err)
+        }
+        else{
+            console.log("connected to database!")
+        }
+
+    }
+);
+// register view engine
+//app.set('view engine','ejs')
 // listen for request
 
 app.listen(3000)
 
-app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root: __dirname})
+app.get("/", (req, res) => {
+    res.sendFile(PATH + "index.html")
 })
 
-app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html', {root: __dirname})
+app.post('/main',(req,res)=>{
+   var topic=req.body.id
+   var data= req.body.data
+   var timestamp= new Date()
+
+   var sql = `INSERT INTO posts (topic, data, timestamp) VALUES ('${topic}', '${data}', '${timestamp}')`
+
+connector.query(sql, (err)=>{
+    if(err){
+        throw (err)
+    }
+    else{
+        console.log("values have been inserted !")
+    }
+
 })
 
-// 404's HAS TO BE AT THE BOTTOM -- LIKE A CATCH ALL 
-app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname})
+res.send("stored successfully !");
+
+})
+
+
+app.get('/getMethod',(req,res)=>{
+var sql='SELECT topic,data,timestamp FROM posts'
+connector.query(sql, (err,result)=>{
+    if(err){
+        throw (err)
+    }
+    else{
+        console.log("values have been posted !")
+        res.json(result)
+    }
+
+})
 })
